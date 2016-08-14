499679fd0753e5ce2cc3a7a5e40c6973c2acab8a
yogurt1 <stalinswag1@gmail.com>
2016-08-15 02:55:48 +0300

    initial browser form validation

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('./lib/globalize');
 const {PORT, HOST} = process.env;
 
 const app = require('./lib/app');
-app.listen(PORT, HOST, function() {
-  console.log(`Express listening on port ${PORT}`);
-});
+const {createServer} = require('http');
+
+server = createServer(app);
+server.listen(PORT, HOST, ev =>
+  console.log(`Express listening on port ${HOST}:${PORT}`)
+);
+server.on('error', (e) => console.log(`Got error: ${e.message}`));
--- models/task/validate.js
+++ models/task/validate.js
@@ -1,7 +1,11 @@
 const schema = require('./schema');
 const ajv = require('ajv')();
+const {toNumber} = require('lodash')
 
 function validate(json) {
+  //const task = toNumber(json.task);
+  //task != 0 ? task : json.task //workaround for number in string
+  json.task = parseInt(json.task);
   return new Promise((resolve, reject) => ajv.validate(schema, json)
     ? resolve(json)
     : reject(ajv.errorsText())
