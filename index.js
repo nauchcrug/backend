f4fbe121d7e49de3af9f54f5e028e31156a02793
yogurt1 <stalinswag1@gmail.com>
2016-08-15 13:13:30 +0300

    TDD fixes

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 require('app-module-path').addPath(process.cwd());
-require('./lib/sql').hook();
+require('lib/sql').hook();
 
-if (process.env.NODE_ENV != 'production') {
-  const dotenv = require('dotenv');
-  dotenv.config();
-}
-
-const {NODE_ENV, PORT, HOST} = process.env;
+if (process.env.NODE_ENV != 'production') require('dotenv/config')
+const {NODE_ENV, PORT, HOST, npm_lifecycle_event} = process.env;
 global.production = (NODE_ENV === 'production');
 
-const app = require('./lib/app');
-const {createServer} = require('http');
-
-server = createServer(app);
-server.listen(PORT, HOST, ev =>
-  console.log(`Express listening on port ${HOST}:${PORT}`)
-);
-//server.on('error', (e) => console.log(`Got error: ${e.message}`));
+if (npm_lifecycle_event != 'test') {
+  const app = require('lib/app');
+  app.listen(PORT, HOST, () =>
+    console.log(`Express listening on ${HOST}:${PORT}`)
+  );
+}
--- test/db.js
+++ test/db.js
@@ -1,11 +1,11 @@
+require('..');
 const {expect} = require('chai');
-require('../lib/globalize');
-const db = require('../lib/db');
+const {db} = require('lib/db');
 
-describe('DI of models', () => {
-  ['user', 'exam'].forEach(name => {
-    it(`type of model ${name} should be object`, () => {
-      let model = require(models + name);
+describe('Models', () => {
+  ['task'].forEach(name => {
+    it(`model \'${name}\' should be object`, () => {
+      let model = require(`models/${name}`);
       expect(model).to.be.an('object');
     });
   });
