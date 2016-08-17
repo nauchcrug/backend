4de00ae5e3275774e1d5706a2ba5553f22bc16a2
maxdegtyarev <max.degtyareff@yandex.ru>
2016-08-17 04:25:04 -0700

    Updated error

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,22 +1,25 @@
-function errorHandlerMiddleware(err, req, res, next) {
-  res.status(err.status || 500)
-  req.headers.accept === 'application/json'
-    ? res.json({message: err.message})
-    : res.render('error', {err})
-  next(); // Last middleware, just for fun :-)
-}
-
-function NotFoundMiddleware(req, res, next) {
+function NotFound(req,res,next){
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 }
 
-function errorMiddlewareFactory() {
-  return [
-    NotFoundMiddleware,
-    errorHandlerMiddleware
-  ];
+function ErrorHandler(err,req,res,next){
+    res.status(err.status || 500)
+    req.headers.accept === 'application/json'
+        ? res.json({message: err.message})
+        : res.render('site/error', {
+        errorTitle : err.status,
+        errorCode: 'Произошла ошибка. Попробуйте вернуться на главную страницу'
+    });
+    next();
 }
 
-module.exports = errorMiddlewareFactory;
+function exporter(){
+    return [
+        NotFound,
+        ErrorHandler
+    ];
+}
+
+module.exports = exporter;
