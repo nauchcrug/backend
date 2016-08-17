4e417d7ac15d8932d8dc443c07f261bab3e1a936
yogurt1 <stalinswag1@gmail.com>
2016-08-17 23:54:26 +0300

    error: fix img

---
+++ test/models.js
@@ -0,0 +1,12 @@
+require('..');
+const {expect} = require('chai');
+//const {db} = require('lib/db');
+
+describe('Models', () => {
+  ['task'].forEach(name => {
+    it(`model \'${name}\' should be object`, () => {
+      let model = require(`models/${name}`);
+      expect(model).to.be.an('object');
+    });
+  });
+});
