5614c6a9feeeed87e738f7238596e81ecfc44fbf
yogurt1 <stalinswag1@gmail.com>
2016-08-23 20:53:07 +0300

    stats route

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -6,5 +6,6 @@ function genRoute(verb, endpoint, basepath) {}
 router.get('/', (req, res) => res.render('cab/user/profile'));
 router.get('/login', (req, res) => res.render('cab/user/login'));
 router.get('/register', (req, res) => res.render('cab/user/register'));
+router.get('/stats', (req, res) => res.render('cab/user/modstat'));
 
 module.exports = router;
