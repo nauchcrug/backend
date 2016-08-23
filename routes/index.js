9590edc0572bf8ef695d71d82a02a70f4659c94f
yogurt1 <stalinswag1@gmail.com>
2016-08-23 14:45:49 +0300

    Git merge conflict broken all code :(. Now fixed :-)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const {Router} = require('express');
 const router = new Router;
->>>>>>> db06bb1a31dda74104ee09487c77ed2d9e15489b
 
 router.use('/', require('./site'));
 router.use('/api', require('./api'));
