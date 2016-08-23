f708b0af713da85fb91673658b49c5e5aa0b4eb1
Maxim <max.degtyareff@yandex.ru>
2016-08-23 15:33:59 -0700

    Tryining fix bug

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -6,8 +6,8 @@ function vendor(name) {
   return serve(`node_modules/${name}`);
 }
 
-const profile = require('./profile');
-router.use('/profile', profile);
+//const profile = require('./profile');   Разкомменить
+//router.use('/profile', profile);
 router.use('/static', [
   serve('node_modules/admin-lte'),
   serve('node_modules/tinymce')
@@ -17,6 +17,6 @@ router.get('/', (req, res) => res.render('cab/index'));
 router.get('/register', (req, res) => res.render('cab/addUser'));
 router.get('/add', (req, res) => res.render('cab/add'));
 router.get('/table', (req, res) => res.render('cab/table'));
-
+router.get('/profile', (req, res) => res.render('cab/profile'));
 module.exports = router;
 
