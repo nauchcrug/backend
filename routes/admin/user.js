9c28d3eed1a4238471ab927970866a794d8b2d93
RISEZID <kzhelnov19@yandex.ru>
2016-08-24 21:45:44 +0300

    Edi rout

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -7,6 +7,6 @@ router.get('/', (req, res) => res.render('cab/user/profile'));
 router.get('/login', (req, res) => res.render('cab/user/login'));
 router.get('/register', (req, res) => res.render('cab/user/register'));
 router.get('/stats', (req, res) => res.render('cab/user/modstat'));
-router.get('/control', (req, res) => res.render('cab/user/controlteam'));
+router.get('/control', (req, res) => res.render('cab/controlteam'));
 
 module.exports = router;
