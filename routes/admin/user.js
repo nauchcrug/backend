323d1bd54488fee2a58c4a6e55174568a58e14bc
RISEZID <kzhelnov19@yandex.ru>
2016-08-24 16:36:56 +0300

    Add link, create control page and add rout

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -7,5 +7,6 @@ router.get('/', (req, res) => res.render('cab/user/profile'));
 router.get('/login', (req, res) => res.render('cab/user/login'));
 router.get('/register', (req, res) => res.render('cab/user/register'));
 router.get('/stats', (req, res) => res.render('cab/user/modstat'));
+router.get('/control', (req, res) => res.render('cab/user/controlteam'));
 
 module.exports = router;
