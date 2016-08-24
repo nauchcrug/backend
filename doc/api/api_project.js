8c0b601efc0d4409d7444fd11146de70b2ef3e26
yogurt1 <stalinswag1@gmail.com>
2016-08-24 16:49:57 +0300

    Fixed indent

--- doc/api/api_project.js
+++ doc/api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-08-23T14:54:32.200Z",
+    "time": "2016-08-24T08:15:36.397Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
