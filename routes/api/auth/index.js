5f3c58a966eca6cfa83e99d9adbaa8deb5684142
Maxim <max.degtyareff@yandex.ru>
2016-09-03 19:03:32 -0700

    Third

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -4,19 +4,25 @@ const {auth, passport} = require('middlewares/auth');
 //const needAuth = require(middlewares + 'needAuth');
 
 router.use(auth);
-router.get('/logout', (req, res) => {
-  res.logout();
-  res.redirect('/auth');
-});
 
-router.get('/callback', passport.authenticate('auth0', {
-  failureRedirect: '/'
-}, (req, res) => {
-  res.render('cab/index',{
-        pageTitle: 'Главная',
-        fullname: req.user.name.givenName + ' ' + req.user.name.familyName
-  });
-}));
+// router.get('/logout', (req, res) => {
+//   res.logout();
+//   res.redirect('/auth');
+// });
 
+//
+// router.get('/callback', passport.authenticate('auth0', {
+//   failureRedirect: '/'
+// }, (req, res) => {
+//   res.render('cab/index',{
+//         pageTitle: 'Главная',
+//         fullname: req.user.name.givenName + ' ' + req.user.name.familyName
+//   });
+// }));
+
+router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/'}),function (err,req,res) {
+    if (err) throw err;
+    res.redirect('/cab');
+})
 //router.post('/register', register);
 module.exports = router;
