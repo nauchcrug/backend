6513cf7ef22c2b5602de0b07e67875851acd13a5
Maxim <max.degtyareff@yandex.ru>
2016-09-03 19:06:44 -0700

    Fix4

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -20,9 +20,12 @@ router.use(auth);
 //   });
 // }));
 
-router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/'}),function (err,req,res) {
-    if (err) throw err;
+router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/'}),function (req,res) {
+    if (!req.user) {
+        throw new Error('user null');
+    }else{
     res.redirect('/cab');
+    }
 })
 //router.post('/register', register);
 module.exports = router;
